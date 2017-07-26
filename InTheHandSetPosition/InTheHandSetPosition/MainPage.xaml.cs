dddb43d4eaf6d582d7162445c218d75f23292372
Matthew Burke <matthewpatrickburke@gmail.com>
2017-07-26 15:40:57 +0100

    initial

---
+++ InTheHandSetPosition/InTheHandSetPosition/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+using InTheHand.Forms;
+
+namespace InTheHandSetPosition
+{
+    public partial class MainPage : ContentPage
+    {
+        public MediaElement mediaView = new MediaElement();
+        public MainPage()
+        {
+            //InitializeComponent();
+            var stack = new StackLayout();
+            
+            mediaView.HeightRequest = 500.0;
+            mediaView.WidthRequest = 500.0;
+            mediaView.Source = new Uri("https://raw.githubusercontent.com/mediaelement/mediaelement-files/master/big_buck_bunny.mp4");
+            mediaView.AreTransportControlsEnabled = true;
+            var button = new Button();
+            button.Text = "Set Position";
+            button.Clicked += Button_Clicked;
+            
+            stack.Children.Add(mediaView);
+            stack.Children.Add(button);
+
+            Content = stack;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var randomNumberGenerator = new System.Random();
+            var rand = randomNumberGenerator.NextDouble();
+            mediaView.Position = TimeSpan.FromSeconds(10.0+rand); // only works on android and windows not iOS
+            //mediaView.Position = TimeSpan.FromSeconds(10.0); //doesn't work on any platform
+        }
+    }
+}
